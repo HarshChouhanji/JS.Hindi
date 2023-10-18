3570ee71dcb6a0977d5ce2eaaa86001eacea6ca5
HarshChouhanji <147059998+HarshChouhanji@users.noreply.github.com>
2023-10-18 11:39:34 +0000

    Datatype

---
+++ Datatype.js
@@ -0,0 +1,19 @@
+console.log("Kanha ji");
+
+let name = "Kanha ji"
+let age = 19
+let isLoggedIn =false
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean => true/ false
+// null => standlone value
+//undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined) //undefined 
+console.log(typeof null) //object
